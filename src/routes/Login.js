3cbdf2e7350be6d443b9511fc9d6adb7d1d32aab
zzuzzu0205 <zkakcnd@naver.com>
2023-01-03 20:03:33 +0900

    'header'

---
+++ src/routes/Login.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import { propTypes } from "react-bootstrap/esm/Image";
+import { useParams } from "react-router-dom";
+import "../App.css";
+
+function Login() {
+  return (
+    <div className="container">
+      <div className="alert alert-warning">2초이내 구입시 할인</div>
+    </div>
+  );
+}
+
+export default Login;
